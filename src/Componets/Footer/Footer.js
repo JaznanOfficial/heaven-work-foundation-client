148251aba8d073f4c9b4678de1fcc17ff60112a8
JaznanOfficial <jaznanofficial@gmail.com>
2021-12-29 07:57:49 +0600

    some changes

--- src/Componets/Footer/Footer.js
+++ src/Componets/Footer/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
 import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className='footer'>
+        <Row className='footer d-flex flex-lg-row'>
+            <Col sm={4} className="left-footer">
+                <h1 className='heaven text-light rounded-pill m-3'>HEAVEN<span className='work text-success m-0 p-0'>WORK</span></h1>
+                <p className='text-light m-3'>A passionate group of people who wishes to inspire a nation and beyond. Bidyanondo is an educational voluntary organization that originated from Bangladesh.</p>
+            </Col>
+            <Col sm={4} className="right-footer">
             
-        </div>
+            </Col>
+            <Col sm={4} className="right-footer">
+            
+            </Col>
+        </Row>
     );
 };
 
