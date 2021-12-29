75d3048947e003b513378321bee6dcbd341dcbc2
JaznanOfficial <jaznanofficial@gmail.com>
2021-12-29 07:28:47 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './Componets/Navigation/Navigation';
 import Homepage from './Componets/Homepage/Homepage';
+import Footer from './Componets/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
       <Homepage></Homepage>
+      <Footer></Footer>
     </div>
   );
 }
